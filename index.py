7342fb961d52cd69cd535428a7866d80a60abc6f
Nirator78 <nirator.dev@gmail.com>
2023-02-15 16:48:24 +0100

    delete improt inutile plus commentaire plus formulaire dans un template

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template, send_file
+from flask import Flask, request, redirect, render_template, send_file
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = './images'
@@ -19,30 +19,17 @@ def allowed_file(filename):
 @app.route('/image/create', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect("http://localhost:5000/infos/"+filename)
         
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data url=/image/create>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template("form.html")
         
 @app.route('/get-image/<filename>')
 def display_image(filename):
@@ -51,4 +38,5 @@ def display_image(filename):
 
 @app.route('/infos/<filename>')
 def infos_for_image(filename):
+    # Mettre ici le programme IA pour retourner la lettre
     return render_template("index.html", user_image = "http://localhost:5000/get-image/"+filename, lettre="A")
